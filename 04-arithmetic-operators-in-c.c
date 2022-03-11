c314d1698d39b5a1d1a8e907c1659b3bd77cdd95
techchandu <94276232+vcsreddy2004@users.noreply.github.com>
2022-03-11 15:49:36 +0530

    Add files via upload

---
+++ 04-arithmetic-operators-in-c.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    printf("Enter first value");
+    scanf("%d",&a);
+    printf("Enter second value");
+    scanf("%d",&b);
+    printf("two operands are %d and %d\n",a,b);
+    printf("sum of %d and %d is %d\n",a,b,a+b);
+    printf("difference between %d and %d is %d\n",a,b,a-b);
+    printf("product of %d and %d is %d\n",a,b,a*b);
+    printf("division of %d and %d is %d\n",a,b,a/b);
+    printf("modulo division of %d and %d is %d\n",a,b,a%b);
+    /*
+        If you want to learn more on c programing
+        you can learn frome this play list it explains in telugu
+        playlist link:https://www.youtube.com/playlist?list=PLGONh1NIhbrmE6iz04IziNKMe5uF9WgVC
+    */
+}
---
+++ 05-conditional-oprators-in-c.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    printf("Enter first value");
+    scanf("%d",&a);
+    printf("Enter second value");
+    scanf("%d",&b);
+    printf("numbers are %d and %d\n",a,b);
+    printf("both are equal:%d\n",a==b);
+    printf("both are not equal:%d\n",a!=b);
+    printf("%d is greater than %d:%d\n",a,b,a>b);
+    printf("%d is less than %d:%d\n",a,b,a<b);
+    printf("%d is greater than or equal to %d:%d\n",a,b,a>=b);
+    printf("%d is less than equal to %d:%d\n",a,b,a<=b);
+    /*
+        If you want to learn more on c programing
+        you can learn frome this play list it explains in telugu
+        playlist link:https://www.youtube.com/playlist?list=PLGONh1NIhbrmE6iz04IziNKMe5uF9WgVC
+    */
+}
